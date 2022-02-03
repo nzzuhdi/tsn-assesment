29a1398ff0808e4648b0ea509b373cf1c8f7931f
nawfal zuhdi <nawfalzuhdi@gmail.com>
2022-02-03 14:14:29 +0700

    setup config & express

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const connect = require("./config/mongodb");
+const cors = require("cors");
+const app = express();
+const port = 3000 || process.env.PORT;
+app.use([cors(), express.urlencoded({ extended: true }), express.json()]);
+
+connect().then(async () => {
+  app.listen(port, () => {
+    console.log("listening on " + port);
+  });
+});
